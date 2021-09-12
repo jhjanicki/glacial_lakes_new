81db29fea6822cc5b2f3cada03a6f66a4970ffaa
Julia <>
2021-09-12 21:56:15 +0200

    first commit

---
+++ data/data.js
@@ -0,0 +1,472 @@
+var data = [
+    {
+        "Latitude": 27.78143902,
+        "Longitude": 87.94596166,
+        "Basin": "Koshi",
+        "Sub_Basin": "Tamor",
+        "Area": 0.05,
+        "Elevation": 4862,
+        "Hazard_Rank": "I",
+        "Country": "Nepal"
+    },
+    {
+        "Latitude": 27.79081457,
+        "Longitude": 87.93442118,
+        "Basin": "Koshi",
+        "Sub_Basin": "Tamor",
+        "Area": 0.15,
+        "Elevation": 4934,
+        "Hazard_Rank": "III",
+        "Country": "Nepal"
+    },
+    {
+        "Latitude": 27.69488134,
+        "Longitude": 87.89325649,
+        "Basin": "Koshi",
+        "Sub_Basin": "Tamor",
+        "Area": 0.02,
+        "Elevation": 4571,
+        "Hazard_Rank": "III",
+        "Country": "Nepal"
+    },
+    {
+        "Latitude": 27.81639563,
+        "Longitude": 87.74955991,
+        "Basin": "Koshi",
+        "Sub_Basin": "Tamor",
+        "Area": 0.18,
+        "Elevation": 4902,
+        "Hazard_Rank": "I",
+        "Country": "Nepal"
+    },
+    {
+        "Latitude": 27.70534638,
+        "Longitude": 87.59657878,
+        "Basin": "Koshi",
+        "Sub_Basin": "Arun",
+        "Area": 0.03,
+        "Elevation": 4772,
+        "Hazard_Rank": "I",
+        "Country": "Nepal"
+    },
+    {
+        "Latitude": 27.72929313,
+        "Longitude": 87.63283426,
+        "Basin": "Koshi",
+        "Sub_Basin": "Arun",
+        "Area": 0.03,
+        "Elevation": 4938,
+        "Hazard_Rank": "III",
+        "Country": "Nepal"
+    },
+    {
+        "Latitude": 27.9260515,
+        "Longitude": 87.77193681,
+        "Basin": "Koshi",
+        "Sub_Basin": "Arun",
+        "Area": 1.01,
+        "Elevation": 4909,
+        "Hazard_Rank": "I",
+        "Country": "China"
+    },
+    {
+        "Latitude": 28.09329815,
+        "Longitude": 87.63665637,
+        "Basin": "Koshi",
+        "Sub_Basin": "Arun",
+        "Area": 0.8,
+        "Elevation": 5177,
+        "Hazard_Rank": "I",
+        "Country": "China"
+    },
+    {
+        "Latitude": 28.05218945,
+        "Longitude": 87.62696914,
+        "Basin": "Koshi",
+        "Sub_Basin": "Arun",
+        "Area": 0.19,
+        "Elevation": 5066,
+        "Hazard_Rank": "I",
+        "Country": "China"
+    },
+    {
+        "Latitude": 28.17858355,
+        "Longitude": 87.56322718,
+        "Basin": "Koshi",
+        "Sub_Basin": "Arun",
+        "Area": 1.12,
+        "Elevation": 5011,
+        "Hazard_Rank": "III",
+        "Country": "China"
+    },
+    {
+        "Latitude": 28.22969917,
+        "Longitude": 87.59108863,
+        "Basin": "Koshi",
+        "Sub_Basin": "Arun",
+        "Area": 0.87,
+        "Elevation": 5410,
+        "Hazard_Rank": "II",
+        "Country": "China"
+    },
+    {
+        "Latitude": 27.94991083,
+        "Longitude": 87.93072237,
+        "Basin": "Koshi",
+        "Sub_Basin": "Arun",
+        "Area": 0.84,
+        "Elevation": 5103,
+        "Hazard_Rank": "I",
+        "Country": "China"
+    },
+    {
+        "Latitude": 27.92863533,
+        "Longitude": 88.00228979,
+        "Basin": "Koshi",
+        "Sub_Basin": "Arun",
+        "Area": 1.11,
+        "Elevation": 5336,
+        "Hazard_Rank": "I",
+        "Country": "China"
+    },
+    {
+        "Latitude": 27.9280083,
+        "Longitude": 88.01956455,
+        "Basin": "Koshi",
+        "Sub_Basin": "Arun",
+        "Area": 0.15,
+        "Elevation": 5692,
+        "Hazard_Rank": "I",
+        "Country": "China"
+    },
+    {
+        "Latitude": 27.93362133,
+        "Longitude": 88.06609303,
+        "Basin": "Koshi",
+        "Sub_Basin": "Arun",
+        "Area": 0.96,
+        "Elevation": 5557,
+        "Hazard_Rank": "I",
+        "Country": "China"
+    },
+    {
+        "Latitude": 27.94623712,
+        "Longitude": 88.07540476,
+        "Basin": "Koshi",
+        "Sub_Basin": "Arun",
+        "Area": 1.48,
+        "Elevation": 5484,
+        "Hazard_Rank": "I",
+        "Country": "China"
+    },
+    {
+        "Latitude": 28.01736003,
+        "Longitude": 88.28806125,
+        "Basin": "Koshi",
+        "Sub_Basin": "Arun",
+        "Area": 0.51,
+        "Elevation": 5268,
+        "Hazard_Rank": "II",
+        "Country": "China"
+    },
+    {
+        "Latitude": 28.37403319,
+        "Longitude": 86.30474524,
+        "Basin": "Koshi",
+        "Sub_Basin": "Arun",
+        "Area": 4.02,
+        "Elevation": 5344,
+        "Hazard_Rank": "II",
+        "Country": "China"
+    },
+    {
+        "Latitude": 28.06928371,
+        "Longitude": 87.13439322,
+        "Basin": "Koshi",
+        "Sub_Basin": "Arun",
+        "Area": 0.28,
+        "Elevation": 4879,
+        "Hazard_Rank": "I",
+        "Country": "China"
+    },
+    {
+        "Latitude": 27.82933493,
+        "Longitude": 87.09530388,
+        "Basin": "Koshi",
+        "Sub_Basin": "Arun",
+        "Area": 0.12,
+        "Elevation": 5208,
+        "Hazard_Rank": "II",
+        "Country": "Nepal"
+    },
+    {
+        "Latitude": 27.79802206,
+        "Longitude": 87.09201073,
+        "Basin": "Koshi",
+        "Sub_Basin": "Arun",
+        "Area": 1.89,
+        "Elevation": 4530,
+        "Hazard_Rank": "I",
+        "Country": "Nepal"
+    },
+    {
+        "Latitude": 27.71114681,
+        "Longitude": 86.97735238,
+        "Basin": "Koshi",
+        "Sub_Basin": "Dudh Koshi",
+        "Area": 0.05,
+        "Elevation": 4636,
+        "Hazard_Rank": "I",
+        "Country": "Nepal"
+    },
+    {
+        "Latitude": 27.75500452,
+        "Longitude": 86.95863667,
+        "Basin": "Koshi",
+        "Sub_Basin": "Dudh Koshi",
+        "Area": 0.86,
+        "Elevation": 4936,
+        "Hazard_Rank": "II",
+        "Country": "Nepal"
+    },
+    {
+        "Latitude": 27.78337497,
+        "Longitude": 86.95769932,
+        "Basin": "Koshi",
+        "Sub_Basin": "Dudh Koshi",
+        "Area": 0.87,
+        "Elevation": 5197,
+        "Hazard_Rank": "I",
+        "Country": "Nepal"
+    },
+    {
+        "Latitude": 27.83803525,
+        "Longitude": 86.93586963,
+        "Basin": "Koshi",
+        "Sub_Basin": "Dudh Koshi",
+        "Area": 0.27,
+        "Elevation": 5201,
+        "Hazard_Rank": "I",
+        "Country": "Nepal"
+    },
+    {
+        "Latitude": 27.85024646,
+        "Longitude": 86.92865018,
+        "Basin": "Koshi",
+        "Sub_Basin": "Dudh Koshi",
+        "Area": 0.45,
+        "Elevation": 5394,
+        "Hazard_Rank": "I",
+        "Country": "Nepal"
+    },
+    {
+        "Latitude": 27.83289983,
+        "Longitude": 86.91721382,
+        "Basin": "Koshi",
+        "Sub_Basin": "Dudh Koshi",
+        "Area": 0.37,
+        "Elevation": 5359,
+        "Hazard_Rank": "I",
+        "Country": "Nepal"
+    },
+    {
+        "Latitude": 27.68751263,
+        "Longitude": 86.85880667,
+        "Basin": "Koshi",
+        "Sub_Basin": "Dudh Koshi",
+        "Area": 0.31,
+        "Elevation": 4762,
+        "Hazard_Rank": "I",
+        "Country": "Nepal"
+    },
+    {
+        "Latitude": 27.89852546,
+        "Longitude": 86.92509563,
+        "Basin": "Koshi",
+        "Sub_Basin": "Dudh Koshi",
+        "Area": 1.34,
+        "Elevation": 5002,
+        "Hazard_Rank": "I",
+        "Country": "Nepal"
+    },
+    {
+        "Latitude": 27.77912847,
+        "Longitude": 86.61236168,
+        "Basin": "Koshi",
+        "Sub_Basin": "Dudh Koshi",
+        "Area": 1.22,
+        "Elevation": 4829,
+        "Hazard_Rank": "I",
+        "Country": "Nepal"
+    },
+    {
+        "Latitude": 27.86101962,
+        "Longitude": 86.47661862,
+        "Basin": "Koshi",
+        "Sub_Basin": "Tama Koshi",
+        "Area": 1.6,
+        "Elevation": 4550,
+        "Hazard_Rank": "I",
+        "Country": "Nepal"
+    },
+    {
+        "Latitude": 27.94679232,
+        "Longitude": 86.44713161,
+        "Basin": "Koshi",
+        "Sub_Basin": "Tama Koshi",
+        "Area": 1.72,
+        "Elevation": 5042,
+        "Hazard_Rank": "I",
+        "Country": "China"
+    },
+    {
+        "Latitude": 28.03351711,
+        "Longitude": 86.50028437,
+        "Basin": "Koshi",
+        "Sub_Basin": "Tama Koshi",
+        "Area": 0.6,
+        "Elevation": 5053,
+        "Hazard_Rank": "II",
+        "Country": "China"
+    },
+    {
+        "Latitude": 28.07317551,
+        "Longitude": 86.52051698,
+        "Basin": "Koshi",
+        "Sub_Basin": "Tama Koshi",
+        "Area": 0.24,
+        "Elevation": 5213,
+        "Hazard_Rank": "I",
+        "Country": "China"
+    },
+    {
+        "Latitude": 28.13503917,
+        "Longitude": 86.53080777,
+        "Basin": "Koshi",
+        "Sub_Basin": "Tama Koshi",
+        "Area": 1,
+        "Elevation": 4984,
+        "Hazard_Rank": "II",
+        "Country": "China"
+    },
+    {
+        "Latitude": 28.18536671,
+        "Longitude": 86.53205904,
+        "Basin": "Koshi",
+        "Sub_Basin": "Tama Koshi",
+        "Area": 0.68,
+        "Elevation": 5025,
+        "Hazard_Rank": "I",
+        "Country": "China"
+    },
+    {
+        "Latitude": 28.24001435,
+        "Longitude": 86.36495846,
+        "Basin": "Koshi",
+        "Sub_Basin": "Tama Koshi",
+        "Area": 0.25,
+        "Elevation": 5327,
+        "Hazard_Rank": "I",
+        "Country": "China"
+    },
+    {
+        "Latitude": 28.19400984,
+        "Longitude": 86.31441546,
+        "Basin": "Koshi",
+        "Sub_Basin": "Tama Koshi",
+        "Area": 0.28,
+        "Elevation": 5245,
+        "Hazard_Rank": "I",
+        "Country": "China"
+    },
+    {
+        "Latitude": 28.30366184,
+        "Longitude": 86.15740489,
+        "Basin": "Koshi",
+        "Sub_Basin": "Sun Koshi",
+        "Area": 0.62,
+        "Elevation": 5306,
+        "Hazard_Rank": "I",
+        "Country": "China"
+    },
+    {
+        "Latitude": 28.34694108,
+        "Longitude": 86.22549892,
+        "Basin": "Koshi",
+        "Sub_Basin": "Sun Koshi",
+        "Area": 0.58,
+        "Elevation": 5348,
+        "Hazard_Rank": "II",
+        "Country": "China"
+    },
+    {
+        "Latitude": 28.36000003,
+        "Longitude": 85.87049392,
+        "Basin": "Koshi",
+        "Sub_Basin": "Sun Koshi",
+        "Area": 4.82,
+        "Elevation": 5209,
+        "Hazard_Rank": "II",
+        "Country": "China"
+    },
+    {
+        "Latitude": 28.32223339,
+        "Longitude": 85.83812927,
+        "Basin": "Koshi",
+        "Sub_Basin": "Sun Koshi",
+        "Area": 5.41,
+        "Elevation": 5067,
+        "Hazard_Rank": "II",
+        "Country": "China"
+    },
+    {
+        "Latitude": 28.16279221,
+        "Longitude": 85.63041261,
+        "Basin": "Gandaki",
+        "Sub_Basin": "Trishuli",
+        "Area": 0.14,
+        "Elevation": 4983,
+        "Hazard_Rank": "I",
+        "Country": "Nepal"
+    },
+    {
+        "Latitude": 28.50859068,
+        "Longitude": 85.4944841,
+        "Basin": "Gandaki",
+        "Sub_Basin": "Trishuli",
+        "Area": 0.28,
+        "Elevation": 4749,
+        "Hazard_Rank": "II",
+        "Country": "China"
+    },
+    {
+        "Latitude": 28.4885156,
+        "Longitude": 84.48552109,
+        "Basin": "Gandaki",
+        "Sub_Basin": "Marsyangdi",
+        "Area": 0.93,
+        "Elevation": 4033,
+        "Hazard_Rank": "I",
+        "Country": "Nepal"
+    },
+    {
+        "Latitude": 29.80249482,
+        "Longitude": 82.67384635,
+        "Basin": "Karnali",
+        "Sub_Basin": "Mugu",
+        "Area": 0.03,
+        "Elevation": 4703,
+        "Hazard_Rank": "II",
+        "Country": "Nepal"
+    },
+    {
+        "Latitude": 30.44562755,
+        "Longitude": 80.38739736,
+        "Basin": "Karnali",
+        "Sub_Basin": "Kali",
+        "Area": 0.12,
+        "Elevation": 4314,
+        "Hazard_Rank": "I",
+        "Country": "India"
+    }
+];
